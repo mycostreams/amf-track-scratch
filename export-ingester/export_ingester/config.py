7c14ff96f0c50377b7215319dbfc506386b8cf80
coco <corentin@bisot.com>
2024-10-21 12:21:08 +0200

    - debugging

--- export-ingester/export_ingester/config.py
+++ export-ingester/export_ingester/config.py
@@ -10,8 +10,13 @@ class Settings(BaseSettings):
     SFTP_HOST: str
     BASE_URL: HttpUrl
     SBATCH_COMMAND: str
-    print(Path(__file__).parent.parent / ".env")
+    env_path = Path(__file__).parent.parent / ".env"
 
+    # Read and print the contents of the file
+    if env_path.exists():
+        with open(env_path, 'r') as file:
+            contents = file.read()
+            print(contents)
     model_config = SettingsConfigDict(
         env_file=Path(__file__).parent.parent / ".env",
         env_file_encoding="utf-8",
