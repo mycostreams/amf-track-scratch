f37e44608a5e0fcdcfec8928493ecabafc914f38
coco <corentin@bisot.com>
2024-10-21 10:07:39 +0200

    - fixed import

--- export-ingester/export_ingester/main.py
+++ export-ingester/export_ingester/main.py
@@ -4,7 +4,8 @@ from datetime import date
 from .config import Settings
 from .ingest import get_managed_export_ingester
 from .models import ExportParams
-
+command = "sbatch /gpfs/home4/mkerrwinter/orchestrator/orchestrator/bash_scripts/downloader.sh" \
+          " /scratch-shared/amftrack2024/daily"
 
 async def main():
     settings = Settings()
@@ -12,7 +13,7 @@ async def main():
     async with get_managed_export_ingester(settings) as export_ingester:
         await export_ingester.ingest(remote, ExportParams())
     async with get_managed_export_ingester(settings) as export_ingester:
-        await export_ingester.run_sbatch_command("sbatch /gpfs/home4/mkerrwinter/orchestrator/orchestrator/bash_scripts/downloader.sh /scratch-shared/amftrack2024/daily")
+        await export_ingester.run_sbatch_command(command)
 
 
 if __name__ == "__main__":
