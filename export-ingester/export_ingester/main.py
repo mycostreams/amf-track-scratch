3701973abf9a6b8ef51f2746cd44cb9cee25d722
coco <corentin@bisot.com>
2024-10-22 19:42:53 +0200

    - Fixed date range to seven days

--- export-ingester/export_ingester/main.py
+++ export-ingester/export_ingester/main.py
@@ -10,8 +10,9 @@ from .utils import get_range
 async def main():
     settings = Settings()
     remote = f"daily-uploads/{date.today()}.json"
-    date_ = (date.today() - timedelta(days=7))
-    start, end = get_range(date_)
+    time_range = 7
+    date_ = (date.today() - timedelta(days=time_range))
+    start, end = get_range(date_,time_range)
     async with get_managed_export_ingester(settings) as export_ingester:
         await export_ingester.ingest(remote, ExportParams(start=start, end=end))
         await export_ingester.run_sbatch_command(settings.SBATCH_COMMAND, remote)
