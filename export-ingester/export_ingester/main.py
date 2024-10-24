3d4ea621476ce51aad4d27e6dc23c29a90d03026
coco <corentin@bisot.com>
2024-10-24 17:08:46 +0200

    - changed remote for test

--- export-ingester/export_ingester/main.py
+++ export-ingester/export_ingester/main.py
@@ -9,7 +9,8 @@ from .utils import get_range
 
 async def main():
     settings = Settings()
-    remote = f"daily-uploads/{date.today()}.json"
+    # remote = f"daily-uploads/{date.today()}.json"
+    remote = f"/scratch-shared/amftrack2024/daily/{date.today()}.json"
     time_range = 7
     date_ = (date.today() - timedelta(days=time_range))
     start, end = get_range(date_,time_range)
