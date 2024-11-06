c3affe856ea845108afc234e8fb449c52cb1ab89
coco <corentin@bisot.com>
2024-11-06 11:47:28 +0100

    - added sbatch to download on snellius

--- export-ingester/export_ingester/archive_export.py
+++ export-ingester/export_ingester/archive_export.py
@@ -9,10 +9,14 @@ from .ingest import get_managed_export_ingester
 async def main(experiment_id: str):
     settings = Settings()
     remote = f"/scratch-shared/amftrack2024/daily/{experiment_id}.json"
+    sbatch_command = ("sbatch /gpfs/home4/mkerrwinter/orchestrator/orchestrator/bash_scripts/downloader_archive.sh"
+     " /scratch-shared/amftrack2024/test_tape")
     async with get_managed_export_ingester(settings) as export_ingester:
         await export_ingester.ingest_archive(
             remote, ArchiveParams(experiment_id=experiment_id)
         )
+        await export_ingester.run_sbatch_command(sbatch_command, remote)
+
 
 
 if __name__ == "__main__":
