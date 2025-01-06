941eb848a1514fcfef31fbc594f46e8e42bafe2f
coco <corentin@bisot.com>
2025-01-06 15:34:28 +0100

    final archiving directly on snellius

--- export-ingester/export_ingester/main.py
+++ export-ingester/export_ingester/main.py
@@ -12,7 +12,7 @@ async def main():
     async with get_managed_export_ingester(settings) as export_ingester:
         # await export_ingester.ingest(remote, ExportParams(start=start, end=end))
         # await export_ingester.run_sbatch_command(settings.SBATCH_COMMAND)
-        archive_command = "srun --time=22:00:00 --partition=staging --nodes=1 --ntasks=1 surf-archiver-cli archive 2024-12-19"
+        archive_command = "sbatch --time=22:00:00 --partition=staging --nodes=1 --ntasks=1 --job-name=surf_archive --output=archive_%j.out --error=archive_%j.err --wrap='surf-archiver-cli archive --mode=images 2024-12-19'"
         async with get_managed_export_ingester(settings) as ingester:
             await ingester.run_sbatch_command(archive_command)
 
